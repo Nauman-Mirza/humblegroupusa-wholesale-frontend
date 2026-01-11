2648e3954bbbb586b47eafce73b58aa04bf51b40
Nauman-Mirza <nmirza403@gmail.com>
2026-01-12 00:45:05 +0500

    redo url

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'https://admin-wholesale.humblegroupusa.com/api/admin';
+const API_BASE = 'http://66.94.118.116/api/admin';
 
 const getToken = () => localStorage.getItem('token');
 
