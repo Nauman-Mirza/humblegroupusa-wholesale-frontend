c403a42d0670cb6d3cf286f5a24276c95e9d4fcb
Nauman-Mirza <nmirza403@gmail.com>
2026-01-12 00:34:14 +0500

    add api over https

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'https://66.94.118.116/api/admin';
+const API_BASE = 'https://admin-wholesale.humblegroupusa.com/api/admin';
 
 const getToken = () => localStorage.getItem('token');
 
